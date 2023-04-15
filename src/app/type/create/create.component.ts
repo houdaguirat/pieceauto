088a478f29325bad2c72aa57e7369fc150df6506
houdaguirat <gkhouda84@gmail.com>
2023-04-15 12:32:50 +0200

    correction

--- src/app/type/create/create.component.ts
+++ src/app/type/create/create.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Type } from '@angular/core';
+import { Router } from '@angular/router';
+import { TypeService } from '../type.service';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  styleUrls: ['./create.component.css'],
 })
-export class CreateComponent {
-
+export class CreateComponent implements OnInit {
+  typeForm: Type = {
+    id: 0,
+    name: '',
+    annee:'',    
+  };
+ 
+  constructor(private ts:TypeService,
+    private router:Router) {}
+ 
+  ngOnInit(): void {}
+ 
+  create(){
+    this.ts.create(this.typeForm)
+    .subscribe({
+      next:(data) => {
+        this.router.navigate(["/Type/home"])
+      },
+      error:(err) => {
+        console.log(err);
+      }
+    })
+  }
 }
